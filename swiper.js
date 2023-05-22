d0bdfa1dde9d5c60d3ea60491172e7ac73ea2180
Leonardo V Oliveira <leonardo.viegaso@gmail.com>
2023-05-22 20:48:41 -0300

    Hub-projetos

---
+++ swiper.js
@@ -0,0 +1,12 @@
+
+const swiper = new Swiper('.swiper', {
+	spaceBetween: 1,
+	slidesPerView: 1,
+	pagination: {
+		el: '.swiper-pagination',
+		type: 'bullets',
+	},
+	autoplay: {
+		delay: 3000,
+	},
+});
